c86f604643c7ece374e5069845785ac3e15d60ba
Arianne Coyco <yhangel.07@gmail.com>
2022-10-08 00:00:58 +0800

    staging environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,8 @@
 
 export const environment = {
   production: false,
-  //server: 'http://localhost:4500',
-  server: 'https://ssr-back.herokuapp.com',
-  main_product_id: 'Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzcxNTE3OTU2MzQzNDk='
+  server: 'http://localhost:3000',
+  main_product_id: 'Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzcxNTE3OTU2MzQzNDk=',
 };
 
 /*
