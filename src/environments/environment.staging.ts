ff8f77e46c77b3691759f6fd544b5622e761370c
Arianne Coyco <yhangel.07@gmail.com>
2022-10-28 16:50:22 +0800

    environment

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   server: 'https://ssr-back.herokuapp.com',
-  main_product_id: 'Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzcxNTE3OTU2MzQzNDk='
+  main_product_id: 'Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzcxNTE3OTU2MzQzNDk=',
+  api_url: 'http://localhost:3000/api'
 };
