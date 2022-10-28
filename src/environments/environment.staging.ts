158ec90be1f6fba24d0376981a9d105dd9d91d0d
Arianne Coyco <yhangel.07@gmail.com>
2022-10-28 23:16:17 +0800

    api link

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   server: 'https://ssr-back.herokuapp.com',
   main_product_id: 'Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzcxNTE3OTU2MzQzNDk=',
-  api_url: 'http://localhost:3000/api'
+  api_url: 'https://staging-api-dot-get-hired-363107.et.r.appspot.com/api'
 };
