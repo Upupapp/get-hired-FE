bb715c0405877df2294cbd030a509c39bee4c44a
Aryhan Coyco <aryhan@upupapp.asia>
2022-12-06 20:48:01 +0800

    doc preview in summary

--- src/app/application/application-process/steps/application-preview/application-preview.component.ts
+++ src/app/application/application-process/steps/application-preview/application-preview.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormGroupDirective, FormArray } from '@angular/forms';
 import { mainAnimations } from '@app-shared/animations/main-animations';
 
 @Component({
@@ -9,11 +10,32 @@ import { mainAnimations } from '@app-shared/animations/main-animations';
 })
 export class ApplicationPreviewComponent implements OnInit {
   @Input() profile: any;
+  govFiles = [];
+  resume = [];
+  coverLetter = [];
+
   public profileSummary: boolean = true;
 
-  constructor() { }
+  constructor(
+    private rootFormGroup: FormGroupDirective,
+  ) { }
+
+  get docGovFile() {
+    return this.rootFormGroup.control.get(['profileDocs', 'governmentFiles']) as FormArray;
+  }
+
+  get docResume() {
+    return this.rootFormGroup.control.get(['profileDocs', 'resume']) as FormArray;
+  }
+
+  get docCover() {
+    return this.rootFormGroup.control.get(['profileDocs', 'coverLetter']) as FormArray;
+  }
 
   ngOnInit(): void {
+    this.govFiles = this.docGovFile.value;
+    this.resume = this.docResume.value;
+    this.coverLetter = this.docCover.value;
   }
 
   viewMore(){
