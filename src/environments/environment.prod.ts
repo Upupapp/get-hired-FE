d524c87a86d359d485a1a481f9baba930b44665a
Aryhan Coyco <aryhan@upupapp.asia>
2023-02-11 04:19:28 +0800

    jobhunt

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   server: 'https://ssr-back.herokuapp.com',
   main_product_id: 'Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzcxNTE3OTU2MzQzNDk=',
-  api_url: 'https://api-dot-jobhunt-375218.ts.r.appspot.com',
+  api_url: 'https://api-dot-jobhunt-375218.ts.r.appspot.com/api',
   app_url: 'https://jobhunt.com.au',
   // api_url: 'https://api-dot-get-hired-363107.et.r.appspot.com/api',
   // app_url: 'http://web.gethiredonline.com.ph',
@@ -10,12 +10,19 @@ export const environment = {
   NgRxMaxAge: 15,
   isDebug: false,
   firebase: {
-    apiKey: 'AIzaSyCn8PyF2eW2sMNsRuzCaOLusCdRcAVpkPY',
-    authDomain: 'get-hired-363107.firebaseapp.com',
-    projectId: 'get-hired-363107',
-    storageBucket: 'get-hired-363107.appspot.com',
-    messagingSenderId: "818317489154",
-    appId: "1:818317489154:web:d849afade1105af929631b",
-    measurementId: "G-4C797NXLJF"
+    // apiKey: 'AIzaSyCn8PyF2eW2sMNsRuzCaOLusCdRcAVpkPY',
+    // authDomain: 'get-hired-363107.firebaseapp.com',
+    // projectId: 'get-hired-363107',
+    // storageBucket: 'get-hired-363107.appspot.com',
+    // messagingSenderId: "818317489154",
+    // appId: "1:818317489154:web:d849afade1105af929631b",
+    // measurementId: "G-4C797NXLJF"
+    apiKey: "AIzaSyDMw_IXIFJkgKE53Fy4UnWwYQ8NhyTHrhI",
+    authDomain: "jobhunt-375218.firebaseapp.com",
+    projectId: "jobhunt-375218",
+    storageBucket: "jobhunt-375218.appspot.com",
+    messagingSenderId: "639015967337",
+    appId: "1:639015967337:web:ef77dedc469057e37be0f8",
+    measurementId: "G-1VRREZSLDP"
   }
 };
