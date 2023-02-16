d2f636df5c5f7e389e59ab79afe2ae60c3edfa3f
Aryhan Coyco <aryhan@upupapp.asia>
2023-02-17 03:54:08 +0800

    eucanna jobs environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,18 +2,18 @@ export const environment = {
   production: true,
   server: 'https://ssr-back.herokuapp.com',
   main_product_id: 'Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzcxNTE3OTU2MzQzNDk=',
-  api_url: 'https://api-dot-get-hired-363107.et.r.appspot.com/api',
-  app_url: 'http://web.gethiredonline.com.ph',
+  // api_url: 'https://api-dot-get-hired-363107.et.r.appspot.com/api',
+  app_url: 'https://web.eucannajobs.com',
   NgRxName: 'App devtools',
   NgRxMaxAge: 15,
   isDebug: false,
   firebase: {
-    apiKey: 'AIzaSyCn8PyF2eW2sMNsRuzCaOLusCdRcAVpkPY',
-    authDomain: 'get-hired-363107.firebaseapp.com',
-    projectId: 'get-hired-363107',
-    storageBucket: 'get-hired-363107.appspot.com',
-    messagingSenderId: "818317489154",
-    appId: "1:818317489154:web:d849afade1105af929631b",
-    measurementId: "G-4C797NXLJF"
+    apiKey: "AIzaSyBHwXzme1n61tjWoZpxfd-3qKcmChKKUBw",
+    authDomain: "eucannajobs-343407.firebaseapp.com",
+    projectId: "eucannajobs-343407",
+    storageBucket: "eucannajobs-343407.appspot.com",
+    messagingSenderId: "62763785485",
+    appId: "1:62763785485:web:954d27008e3c8e3e5da444",
+    measurementId: "G-JJ4TFKMEX0"
   }
 };
