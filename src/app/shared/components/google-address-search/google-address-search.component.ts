0d886acacdc3a93295524c2ddc32253b0b7c7d23
Aryhan Coyco <aryhan@upupapp.asia>
2023-03-09 18:16:15 +0800

    JP-32 Reusable google address search

--- src/app/shared/components/google-address-search/google-address-search.component.ts
+++ src/app/shared/components/google-address-search/google-address-search.component.ts
@@ -1,6 +1,8 @@
-import { AfterViewInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Input, Output, ViewChild } from '@angular/core';
 import { ElementRef } from '@angular/core';
+import { EventEmitter } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { GoogleAddressService } from '@app-shared/services/api/google-address.service';
 
 @Component({
@@ -10,27 +12,93 @@ import { GoogleAddressService } from '@app-shared/services/api/google-address.se
 })
 export class GoogleAddressSearchComponent implements OnInit, AfterViewInit {
   @ViewChild('search') searchElementRef!: ElementRef;
+  @Input() rawAddress: any;
+  @Output() addressChange = new EventEmitter<any>();
+
+  addressFormGroup: FormGroup;
 
   constructor(
-    private googleAddressService: GoogleAddressService
+    private googleAddressService: GoogleAddressService,
+    private formBuilder: FormBuilder
   ) {
     this.googleAddressService.apiLoaded();
   }
 
   ngOnInit(): void {
+    const {
+      address,
+      state,
+      country,
+      addressOne,
+      town,
+      city,
+      zipcode,
+      mapUrl
+    } = this.rawAddress;
+
+    this.addressFormGroup = this.formBuilder.group({
+      address: [address],
+      state: [state],
+      country: [country, Validators.required],
+      addressOne: [addressOne, Validators.required],
+      town: [town],
+      city: [city, Validators.required],
+      zipcode: [zipcode],
+      mapUrl: [mapUrl]
+    })
 
   }
 
   ngAfterViewInit(): void {
     // Binding autocomplete to search input control
+
+
+
     let autocomplete = new google.maps.places.Autocomplete(
-      this.searchElementRef.nativeElement
+      this.searchElementRef.nativeElement,
+      {
+
+        componentRestrictions: { country: ["ph", "sg"] },
+        fields: ["address_components", "url"],
+        types: ["address"]
+      }
     );
+
+    if (this.rawAddress != '') {
+      this.searchElementRef.nativeElement.focus()
+    }
 
     autocomplete.addListener('place_changed', () => {
       const result = autocomplete.getPlace();
       console.log(result)
+
+      const state = this.googleAddressService.getState(result);
+      const country = this.googleAddressService.getCountry(result);
+      const addressOne = this.googleAddressService.getStreetNumber(result) + ' ' + this.googleAddressService.getStreet(result);
+      const sublocal = this.googleAddressService.getTown(result);
+      const neighborhood = this.googleAddressService.getNeighborhood(result);
+      const town = sublocal ? sublocal : neighborhood;
+      const city = this.googleAddressService.getLocality(result);
+      const zipcode = this.googleAddressService.getPostCode(result);
+      const mapUrl = this.googleAddressService.getPostCode(result);
+
+      this.populateForm({
+        state, country, addressOne, town, city, zipcode, mapUrl
+      });
+
+      this.addressChange.emit(this.addressFormGroup.value);
     });
+
+  }
+
+  populateForm(rawAddress) {
+    this.addressFormGroup.get('state').setValue(rawAddress.state);
+    this.addressFormGroup.get('country').setValue(rawAddress.country);
+    this.addressFormGroup.get('addressOne').setValue(rawAddress.addressOne);
+    this.addressFormGroup.get('town').setValue(rawAddress.town);
+    this.addressFormGroup.get('city').setValue(rawAddress.city);
+    this.addressFormGroup.get('zipcode').setValue(rawAddress.zipcode);
+    this.addressFormGroup.get('mapUrl').setValue(rawAddress.mapUrl);
   }
 
 }
