2096d6ff8b4c7d24f761379939330ee6c61b46e3
Aryhan Coyco <aryhan@upupapp.asia>
2023-11-09 06:16:33 +0800

    upgrade to version 15 and add quill

--- src/app/shared/store/effects/company.effect.ts
+++ src/app/shared/store/effects/company.effect.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { catchError, map, switchMap } from 'rxjs/operators';
 
-import { Actions, ofType, Effect, createEffect } from '@ngrx/effects';
+import { Actions, ofType, createEffect } from '@ngrx/effects';
 
 //import enum action types
 import { CompanyActionTypes } from '../actions/company.action';
